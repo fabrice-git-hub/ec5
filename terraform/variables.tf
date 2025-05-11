605fd85784edee5df38ea53f889fc697c126745c
fab <ulukai31@hotmail.com>
2025-05-11 20:00:43 +0200

    variables.tf modification

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,15 @@ variable "ENVIRONMENT_NAME" {
   type = string
   description = "Environnement (test ou prod)"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Clé d'accès AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Clé secrète AWS"
+  type        = string
+  sensitive   = true
+}
